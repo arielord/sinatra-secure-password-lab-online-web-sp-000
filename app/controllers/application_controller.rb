071172da436e044b6f664c87a8bf0104924f3d67
Ariel <arielord@buffalo.edu>
2019-07-11 01:23:58 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,10 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
+    user = User.new(params[:username], params[:password])
     
+    if user.save
+      
   end
 
   get '/account' do
